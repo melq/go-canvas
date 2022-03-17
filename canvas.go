cefa85f29da10e2c33b2e83445c66cd848cf4f71
melq <tu2sice@yahoo.co.jp>
2022-03-17 16:33:33 +0900

    :construction: create toImage

--- canvas.go
+++ canvas.go
@@ -1,24 +1,34 @@
 package canvas
 
+import (
+	"image"
+	"image/color"
+)
+
 type Canvas struct {
-	H    int
 	W    int
-	Data []int
+	H    int
+	Data []color.Color
 }
 
-/*type point struct {
-	x int
-	y int
-}*/
-
-func NewCanvas(h int, w int) *Canvas {
+func NewCanvas(w int, h int) *Canvas {
 	return &Canvas{
-		H:    h,
 		W:    w,
-		Data: make([]int, h*w),
+		H:    h,
+		Data: make([]color.Color, w*h),
 	}
 }
 
-/*func (c *Canvas) Line(s point, e point) {
+func (c *Canvas) toImage() image.Image {
+	img := image.NewRGBA(image.Rect(0, 0, c.W, c.H))
+	for y := 0; y < c.H; y++ {
+		for x := 0; x < c.W; x++ {
+			img.Set(x, y, c.Data[y*c.W+x])
+		}
+	}
+	return img
+}
 
-}*/
+func (c *Canvas) ToPng(filename string) {
+
+}
