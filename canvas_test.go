e866cd71cc16a201fbc5776b8f09a3e96090ece2
melq <tu2sice@yahoo.co.jp>
2022-03-19 17:00:45 +0900

    :star: ペンの色を変えられるようにした

--- canvas_test.go
+++ canvas_test.go
@@ -19,7 +19,12 @@ func TestToPng(t *testing.T) {
 	p1 := image.Point{X: 100, Y: 100}
 	p2 := image.Point{X: 700, Y: 300}
 	c.Line(p1, p2)
+	fmt.Println(c.Color)
+	c.ChangeColor(color.RGBA{R: 200, G: 150, B: 150, A: 255})
 	c.Rect(p1, p2)
+	fmt.Println(c.Color)
+	c.ChangeColor(color.RGBA{R: 150, G: 150, B: 200, A: 255})
 	c.Triangle(image.Point{X: 400, Y: 50}, image.Point{X: 600, Y: 350}, image.Point{X: 200, Y: 350})
+	fmt.Println(c.Color)
 	c.ToPng("./img/sample.png")
 }
